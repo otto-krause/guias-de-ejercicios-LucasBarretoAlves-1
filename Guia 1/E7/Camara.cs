63f9c34ed4b09c650ec3a51285991c34f7c1f250
LucasBarretoAlves <lucasnat2210@gmail.com>
2020-04-20 12:48:06 -0300

    Arreglos minimos en la separacion de varios codigos

--- Guia 1/E7/Camara.cs
+++ Guia 1/E7/Camara.cs
@@ -2,6 +2,20 @@ namespace E7
 {
     public class Camara
     {
-        
+        int megapixeles;
+        int cantidadfotos;
+        public Camara(int megapixeles, int cantidadfotos)
+        {
+            this.megapixeles=megapixeles;
+            this.cantidadfotos=cantidadfotos;
+        }
+        public int pesoPorFoto()
+        {
+            return megapixeles*3;
+        }
+        public int PesoTotalDeFotos()
+        {
+            return this.pesoPorFoto()*cantidadfotos;
+        }
     }
 }
--- Guia 1/E7/Program.cs
+++ Guia 1/E7/Program.cs
@@ -35,50 +35,5 @@ namespace E7
             }
         }
     }
-    class Celular
-    {
-        bool NFC;
-        bool Bluetooth;
-        int almacenamiento;
-        int aplicaciones;
-        Camara camara;
-        Camara camara2;
-        public Celular(bool NFC, bool Bluetooth, int almacenamiento, int aplicaciones,Camara camara, Camara camara2)
-        {
-            this.NFC=NFC;
-            this.Bluetooth=Bluetooth;
-            this.almacenamiento=almacenamiento;
-            this.aplicaciones=aplicaciones;
-            this.camara=camara;
-            this.camara2=camara2;
-        }
-
-        public  int MemoriaDisponible()
-        {
-            return almacenamiento -(camara.PesoTotalDeFotos()+camara2.PesoTotalDeFotos()+aplicaciones*70);
-        }
-
-        public bool TeCargaLaSUBE()
-        {
-            return NFC;
-        }
-    }
-    class Camara
-    {
-        int megapixeles;
-        int cantidadfotos;
-        public Camara(int megapixeles, int cantidadfotos)
-        {
-            this.megapixeles=megapixeles;
-            this.cantidadfotos=cantidadfotos;
-        }
-        public int pesoPorFoto()
-        {
-            return megapixeles*3;
-        }
-        public int PesoTotalDeFotos()
-        {
-            return this.pesoPorFoto()*cantidadfotos;
-        }
-    }
+    
 }
