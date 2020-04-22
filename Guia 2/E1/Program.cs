5c245e5ef4c1b60b358d3b1b3fc6a3e028d9bbf6
LucasBarretoAlves <lucasnat2210@gmail.com>
2020-04-22 17:41:20 -0300

    Subo ejercicio  1 de la Guia 2

--- Guia 2/E1/Program.cs
+++ Guia 2/E1/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace E1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ingreso=1;
+            int primer=0;
+            int ultimo=0;
+            int i=0;
+            int mayores=0;
+            List<int> numeros = new List<int>();
+            while (ingreso!=0)
+            {
+                Console.Write("Ingrese un numero: ");
+                ingreso=Int32.Parse(Console.ReadLine());
+                if (ingreso!=0)
+                {
+                    numeros.Add(ingreso);
+                    ultimo=ingreso;
+                }     
+                if (i==0)
+                    primer=ingreso;
+                if (ingreso>50)
+                    mayores++;
+                i++;
+                
+            }
+            Console.WriteLine("Los numeros de la lista son:");
+            foreach (int num in numeros)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("\nLa cantidad de elementos ingresados fue de " + numeros.Count);
+            Console.WriteLine("El primer numero fue " + primer);
+            Console.WriteLine("El ultimo numero fue " + ultimo);
+            Console.WriteLine("La cantidad de numeros mayores a 50 fue de " + mayores); 
         }
     }
 }
